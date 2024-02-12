a1cf20b319d32de1927b94b6a333d16ce9982931
Tobias Jordan <git@tobiasjordan.io>
2024-02-12 12:01:31 +0100

    ugly

--- src/json_parser.hpp
+++ src/json_parser.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <thread>
+
 #include <sycl/sycl.hpp>
 
 #include "definitions.hpp"
@@ -11,7 +13,7 @@
 // Host -> Bitmap computation.
 class ProducerId;
 class InPipeId;
-using InPipe = sycl::ext::intel::experimental::pipe<InPipeId, CacheLine, PIPELINE_DEPTH>;
+using InPipe = sycl::ext::intel::pipe<InPipeId, CacheLine, PIPELINE_DEPTH>;
 
 // Bitmap computation -> String filter (both device).
 class TokenizerId;
@@ -22,49 +24,72 @@ using TokenizerToStringFilterPipe =
 // String filter -> Tape Builder (Host).
 class StringFilterId;
 class OutPipeId;
-using OutPipe = sycl::ext::intel::experimental::pipe<OutPipeId, OutputCacheLine, PIPELINE_DEPTH>;
+using OutPipe = sycl::ext::intel::pipe<OutPipeId, OutputCacheLine, PIPELINE_DEPTH>;
 
 class ConsumerId;
 
-template <typename Id, typename InPipe>
-std::pair<sycl::event, size_t> submit_producer(sycl::queue &q, const std::string &input) {
+template <typename Id, typename InPipe> size_t submit_producer(sycl::queue &q, const std::string &input) {
 	const auto input_size = input.size();
-
-	// Check if input size is divisible by CACHE_LINE_SIZE
+	const auto cache_line_count = input_size / CACHE_LINE_SIZE;
 	const bool underfull_cache_line = input_size % CACHE_LINE_SIZE != 0;
 
-	const auto cache_line_count = input_size / CACHE_LINE_SIZE;
-	auto input_buffer = sycl::buffer{input};
+	auto produce_cachelines = [](sycl::queue &q, const std::string &input) {
+		const auto input_size = input.size();
+		const auto cache_line_count = input_size / CACHE_LINE_SIZE;
 
-	const auto producer_event = q.submit([&](auto &h) {
-		const auto input_accessor = sycl::accessor{input_buffer, h, sycl::read_only};
+		// Check if input size is divisible by CACHE_LINE_SIZE
+		const bool underfull_cache_line = input_size % CACHE_LINE_SIZE != 0;
 
-		h.template single_task<Id>([=]() {
-			for (auto index = size_t{0}; index < cache_line_count; ++index) {
-				const auto begin = input_accessor.begin() + index * CACHE_LINE_SIZE;
-				const auto end = input_accessor.begin() + (index + 1) * CACHE_LINE_SIZE;
-				auto line = CacheLine{};
-				std::copy(begin, end, line.begin());
-				InPipe::write(line);
-			}
+		for (auto index = size_t{0}; index < cache_line_count; ++index) {
+			const auto begin = input.begin() + index * CACHE_LINE_SIZE;
+			const auto end = input.begin() + (index + 1) * CACHE_LINE_SIZE;
+			auto line = CacheLine{};
+			std::copy(begin, end, line.begin());
+			auto line_buffer = sycl::buffer{line};
 
-			if (underfull_cache_line) {
-				const auto begin = input_accessor.begin() + cache_line_count * CACHE_LINE_SIZE;
-				const auto end = input_accessor.begin() + input_size;
-				auto line = CacheLine{};
-				std::copy(begin, end, line.begin());
-				std::fill(line.begin() + (input_size % CACHE_LINE_SIZE), line.end(), ' ');
-				InPipe::write(line);
-			}
-		});
-	});
+			q.submit([&](auto &h) {
+				const auto line_accessor = sycl::accessor{line_buffer, h, sycl::read_only};
+
+				h.single_task([=]() {
+					const auto begin = line_accessor.begin();
+					const auto end = line_accessor.begin() + CACHE_LINE_SIZE;
+					auto cache_line = CacheLine{};
+					std::copy(begin, end, cache_line.begin());
+					InPipe::write(cache_line);
+				});
+			});
+		}
+		if (underfull_cache_line) {
+			const auto begin = input.begin() + cache_line_count * CACHE_LINE_SIZE;
+			const auto end = input.begin() + input_size;
+			auto line = CacheLine{};
+			std::copy(begin, end, line.begin());
+			std::fill(line.begin() + (input_size % CACHE_LINE_SIZE), line.end(), ' ');
+			auto line_buffer = sycl::buffer{line};
+
+			q.submit([&](auto &h) {
+				const auto line_accessor = sycl::accessor{line_buffer, h, sycl::read_only};
+
+				h.single_task([=]() {
+					const auto begin = line_accessor.begin();
+					const auto end = line_accessor.begin() + CACHE_LINE_SIZE;
+					auto cache_line = CacheLine{};
+					std::copy(begin, end, cache_line.begin());
+					InPipe::write(cache_line);
+				});
+			});
+		}
+	};
+
+	auto producer_thread = std::thread(produce_cachelines, std::ref(q), std::ref(input));
+	producer_thread.detach();
 
 	const auto final_cache_line_count = underfull_cache_line ? cache_line_count + 1 : cache_line_count;
-	return {producer_event, final_cache_line_count};
+	return final_cache_line_count;
 }
 
 TapedJson parse(sycl::queue &q, const std::string &input) {
-	const auto [producer_event, cache_line_count] = submit_producer<ProducerId, InPipe>(q, input);
+	auto cache_line_count = submit_producer<ProducerId, InPipe>(q, input);
 
 	const auto tokenizer_event =
 		submit_tokenizer<TokenizerId, InPipe, TokenizerToStringFilterPipe>(q, cache_line_count);
